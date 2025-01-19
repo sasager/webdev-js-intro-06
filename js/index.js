2e73640cace6054ee171e7e4584f0e03fb7fb841
Sierra <sierra.ar.sager@gmail.com>
2025-01-19 18:47:51 -0500

    completed experiment

--- js/index.js
+++ js/index.js
@@ -1,3 +1,39 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*
+
+for (let i=0; i < 8; i++) {
+    console.log(i);
+}
+
+for (let i=0; i < 8; i++) {
+    if (i === 4) {
+        break;
+    }
+}
+
+for (let i=0; i < 8; i++) {
+    if (i === 4) {
+        continue;
+    }
+}
+
+let i = 3;
+do {
+  i += 2;
+  console.log(i);
+} while (i < 12);
+
+let n = 0;
+let x = 0;
+while (n < 7) {
+  n++;
+  x += n;
+  if (x === 5) {
+    break;
+  }
+}
+
+*/
